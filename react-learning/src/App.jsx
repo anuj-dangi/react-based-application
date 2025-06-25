68bc78165d2ea62ac5c75077a3379cb34a6dee50
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-25 22:14:30 +0530

    second module

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Card from "./components/Card.jsx";
+import Individual from './Individual/Individual.jsx';
 
 function App() {
 
   return (
     <>
-      <Card />
+      <Individual />
     </>
   )
 }
---
+++ react-learning/src/Individual/Individual.jsx
@@ -0,0 +1,10 @@
+import styles from "./Individual.module.css";
+
+function Individual()
+{
+    return (
+        <h1 className={styles.text}>Styling through module in which class name is changed with hashcode</h1>
+    );
+};
+
+export default Individual;
